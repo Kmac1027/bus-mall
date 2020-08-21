ff3984fe7a763f846abdb9933720210ecedc82bf
Kevin McNeil <dogwoodrawks@gmail.com>
2020-08-21 16:25:47 -0700

    minor bug fixes

--- JS/app2.js
+++ JS/app2.js
@@ -28,26 +28,26 @@ function checkLocalStorage() {
 checkLocalStorage();
 
 function createItems() {
-new Item('../IMG/bag.jpg', 'bag', 'bag');
-new Item('../IMG/banana.jpg', 'banana', 'banana');
-new Item('../IMG/bathroom.jpg', 'bathroom', 'bathroom');
-new Item('../IMG/boots.jpg', 'boots', 'boots');
-  var breakfast = new Item('../IMG/breakfast.jpg', 'breakfast', 'breakfast');
-  var bubblegum = new Item('../IMG/bubblegum.jpg', 'bubblegum', 'bubblegum');
-  var chair = new Item('../IMG/chair.jpg', 'chair', 'chair');
-  var cthulhu = new Item('../IMG/cthulhu.jpg', 'cthulhu', 'cthulhu');
-  var dogDuck = new Item('../IMG/dog-duck.jpg', 'dog duck', 'dog duck');
-  var dragon = new Item('../IMG/dragon.jpg', 'dragon', 'dragon');
-  var pen = new Item('../IMG/pen.jpg', 'pen', 'pen');
-  var sweep = new Item('../IMG/pet-sweep.jpg', 'pet sweep', 'pet sweep');
-  var scissors = new Item('../IMG/scissors.jpg', 'scissors', 'scissors');
-  var shark = new Item('../IMG/shark.jpg', 'shark', 'shark');
-  var sweep = new Item('../IMG/sweep.png', 'sweep', 'sweep');
-  var tauntaun = new Item('../IMG/tauntaun.jpg', 'tauntaun', 'tauntaun');
-  var unicorn = new Item('../IMG/unicorn.jpg', 'unicorn', 'unicorn');
-  var usb = new Item('../IMG/usb.gif', 'usb', 'usb');
-  var waterCan = new Item('../IMG/water-can.jpg', 'water-can', 'water-can');
-  var wineGlass = new Item('../IMG/wine-glass.jpg', 'wine glass', 'wine glass')
+  new Item('./../IMG/bag.jpg', 'bag', 'bag');
+  new Item('./../IMG/banana.jpg', 'banana', 'banana');
+  new Item('./../IMG/bathroom.jpg', 'bathroom', 'bathroom');
+  new Item('./../IMG/boots.jpg', 'boots', 'boots');
+  new Item('./../IMG/breakfast.jpg', 'breakfast', 'breakfast');
+  new Item('./../IMG/bubblegum.jpg', 'bubblegum', 'bubblegum');
+  new Item('./../IMG/chair.jpg', 'chair', 'chair');
+  new Item('./../IMG/cthulhu.jpg', 'cthulhu', 'cthulhu');
+  new Item('./../IMG/dog-duck.jpg', 'dog duck', 'dog duck');
+  new Item('./../IMG/dragon.jpg', 'dragon', 'dragon');
+  new Item('./../IMG/pen.jpg', 'pen', 'pen');
+  new Item('./../IMG/pet-sweep.jpg', 'pet sweep', 'pet sweep');
+  new Item('./../IMG/scissors.jpg', 'scissors', 'scissors');
+  new Item('./../IMG/shark.jpg', 'shark', 'shark');
+  new Item('./../IMG/sweep.png', 'sweep', 'sweep');
+  new Item('./../IMG/tauntaun.jpg', 'tauntaun', 'tauntaun');
+  new Item('./../IMG/unicorn.jpg', 'unicorn', 'unicorn');
+  new Item('./../IMG/usb.gif', 'usb', 'usb');
+  new Item('./../IMG/water-can.jpg', 'water-can', 'water-can');
+  new Item('./../IMG/wine-glass.jpg', 'wine glass', 'wine glass')
 }
 
 
